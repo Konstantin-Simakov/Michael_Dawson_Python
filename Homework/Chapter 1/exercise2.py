df5bb31fad3bf4200b675b4fd751a881635a94fb
Konstantin-Simakov <kvs.2000@mail.ru>
2023-04-05 18:39:32 +0300

    Delete Homework directory

--- Homework/Chapter 1/exercise2.py
+++
@@ -1,5 +0,0 @@
-# exercise2.py -- Chapter 1
-# Displays the message about the username
-
-print("Your name is Konstantin.")
-input("\n\nPress the key [Enter] to exit.")
--- Homework/Chapter 1/exercise3.py
+++
@@ -1,7 +0,0 @@
-# exercise3.py -- Chapter 1
-# Displays my favourite aphorism and its author
-
-print("Learn and live.")
-print("Senek.")
-
-input("\n\nPress the key [Enter] to exit.")
