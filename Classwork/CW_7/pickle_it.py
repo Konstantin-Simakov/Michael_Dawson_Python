d408cc842300af5a8db868745bd555fdcfce1a33
Konstantin_Simakov <kvs.2000@mail.ru>
2024-09-23 18:23:45 +0300

    Add files via upload

--- Classwork/CW_7/pickle_it.py
+++ Classwork/CW_7/pickle_it.py
@@ -11,37 +11,38 @@ print("Pickle lists.")
 variety = ["cucumbers", "tomatoes", "cabbage"]
 shape = ["whole", "diced", "straw"]
 brand = ["Main Product", "Chumak", "Bonduel"]
-f = open("picles_1.dat", "wb")
-pickle.dump(variety, f)
-pickle.dump(shape, f)
-pickle.dump(brand, f)
-f.close()
+bin_file = open("pickle_1.dat", "wb+")
+pickle.dump(variety, bin_file)
+pickle.dump(shape, bin_file)
+pickle.dump(brand, bin_file)
+# bin_file.close()
 
 print("\nUnpickle lists.")
-f = open("picles_1.dat", "rb")
-variety = pickle.load(f)
-shape = pickle.load(f)
-brand = pickle.load(f)
+bin_file.seek(0)                                # Return to the beginning of the file without closing it.
+# bin_file = open("pickle_1.dat", "rb")
+variety = pickle.load(bin_file)
+shape = pickle.load(bin_file)
+brand = pickle.load(bin_file)
 print(variety)
 print(shape)
 print(brand)
-f.close()
+bin_file.close()
 
 # 2
 print("\nShelve lists.")
 # Read and write, create the file if it doesn't exist.
-# s = shelve.open("pickle_2.dat", "c")
-s = shelve.open("pickle_2.dat")
-s["variety"] = ["cucumbers", "tomatoes", "cabbage"]
-s["shape"] = ["whole", "diced", "straw"]
-s["brand"] = ["Main product", "Chumak", "Bonduel"]
+# sh = shelve.open("pickle_2.dat", "c")         # "c" == "ab+", "n" == "wb+", "r" == "rb", "w" == "ab"
+sh = shelve.open("pickle_2.dat")
+sh["variety"] = ["cucumbers", "tomatoes", "cabbage"]
+sh["shape"] = ["whole", "diced", "straw"]
+sh["brand"] = ["Main product", "Chumak", "Bonduel"]
 # Make sure data is put down
-s.sync()
+sh.sync()
 
 print("\nUnshelve lists.")
-print("Brands -", s["brand"])
-print("Shapes -", s["shape"])
-print("Varieties -", s["variety"])
-s.close()
+print("Brands -", sh["brand"])
+print("Shapes -", sh["shape"])
+print("Varieties -", sh["variety"])
+sh.close()
 
 input("\n\nPress the key <Enter> to exit.")
